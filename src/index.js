c7464e11f5413968597c4b004d16768c85e23ee2
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-09 11:32:07 +0530

    connecting database way1

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from 'express';
+// const app = express();
+
+
+// ( async () => {
+//     try {
+//      await mongoose.connect(`${MONGODB_URL}/${DB_NAME}`);
+//      app.on('error', (error) => {
+//         console.log("Error: ", error);
+//         throw error
+//      })
+//      app.listen(process.env.PORT, () => {
+//         console.log(`App is listining on port ${process.env.PORT}`)
+//      })
+        
+//     } catch (error) {
+//         console.log('ERROR', error);
+//         throw error
+//     }
+// })()
