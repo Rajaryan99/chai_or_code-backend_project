6a1640b755fa61935b831d14c6996326cef4eb7a
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-09 13:34:35 +0530

    MongoDB connected successfully

---
+++ src/db/index.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/youtube');
+        console.log('Database Connected successfully!!')
+    } catch (error) {
+        console.log("ERROR", error);
+        throw error
+    }
+}
+
+
+
+
+
+// const connectDB = async () => {
+//     try {
+//         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+//         console.log(`\n MongoDB connected !! DB Host: ${connectionInstance.connection.host}`);
+//     } catch (error) {
+//         console.log('MONGODB connection error', error);
+//         process.exit(1);
+//     }
+// }
+
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
+import dotenv from 'dotenv'
+
+dotenv.config({ path: './env' })
+
 import mongoose from 'mongoose';
-import { DB_NAME } from './constants';
+import { DB_NAME } from './constants.js';
+import connectDB from './db/index.js';
+
+
+connectDB();
 
 
 
