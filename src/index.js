162f1d946d6e9db13838c99920009b708a7f5f92
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-10 05:56:44 +0530

    created our server using express

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import dotenv from 'dotenv'
+import connectDB from './db/index.js';
+import { app } from './app.js';
 
 dotenv.config({ path: './env' })
 
-import mongoose from 'mongoose';
-import { DB_NAME } from './constants.js';
-import connectDB from './db/index.js';
+connectDB().then(() => {
 
+    app.on('error', (error) => {
+        console.log('ERROR: ', error);
+        throw error;
+    });
 
-connectDB();
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on http://localhost:${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log('Mongo db connection failed!!', err)
+})
 
 
 
