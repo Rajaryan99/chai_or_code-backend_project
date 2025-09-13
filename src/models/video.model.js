2bb78b0deaf4584fbd013ac80e785ee8dc0ef8db
Rajaryan singh <raj.aryan2518@gmail.com>
2025-09-13 13:33:52 +0530

    inscyption of data uisng bcrypt

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,4 +1,5 @@
 import mongoose, {Schema} from "mongoose";
+import mongooseAggreatePaginate from 'mongoose-aggregate-paginate-v2';
 
 const videoSchema = new Schema(
     {
@@ -47,5 +48,8 @@ const videoSchema = new Schema(
         timestamps: true,
     }
 );
+
+
+videoSchema.plugin(mongooseAggreatePaginate);
 
 export const Video = mongoose.model('Video', videoSchema);
